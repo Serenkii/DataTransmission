ca7417825782c4e34265bc8c68ef147fcab81294
Marian <76261221+Serenkii@users.noreply.github.com>
2021-03-12 21:30:45 +0100

    Add files via upload

--- DataTransmission_0.3/DataTransmission_0.3.ino
+++ DataTransmission_0.3/DataTransmission_0.3.ino
@@ -32,44 +32,6 @@ void loop() {
   delay(5000);
 }
 
-
-byte method3(byte bits) {
-  //Serial.println("method3:");
-  for (int i = 7; i >= 0; i--) {
-    byte tempRandom = random(0,2);
-    //Serial.print(tempRandom);
-    //Serial.print("  ---  ");
-    bits = bits + (tempRandom << i);
-    //printInBinary(bits, true);
-  }
-  return bits;
-}
-
-byte method2(byte bits) {
-  //Serial.println("method2:");
-  for (int i = 0; i < 8; i++) {
-    byte tempRandom = random(0,2);
-    //Serial.print(tempRandom);
-    //Serial.print("  ---  ");
-    bits = bits + ((0x80 >> i) * tempRandom);
-    //printInBinary(bits, true);
-  }
-  return bits;
-}
-
-byte method1(byte bits) {
-  //Serial.println("method1:");
-  for (int i = 0; i < 8; i++) {
-    bits = bits << 1;       //bitshift to the left
-    //printInBinary(bits, false);
-    bits = bits + random(0,2);       
-    //Serial.print("  ---  ");
-    //printInBinary(bits, true);
-  }
-  return bits;
-}
-
-
 void printInBinary(byte byteToPrint, bool line) {
   for (int i = 7; i >= 0; i--) {
     Serial.print(bitRead(byteToPrint, i));
